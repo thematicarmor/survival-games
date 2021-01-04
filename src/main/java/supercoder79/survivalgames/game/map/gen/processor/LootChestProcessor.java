9879285e89d445a3686e9db414299520b96765cc
SuperCoder7979 <k.pranav@gmail.com>
2021-01-03 22:41:52 -0500

    add new houses and add loot to enchanting tables

--- src/main/java/supercoder79/survivalgames/game/map/gen/processor/LootChestProcessor.java
+++ src/main/java/supercoder79/survivalgames/game/map/gen/processor/LootChestProcessor.java
@@ -12,20 +12,24 @@ import net.minecraft.structure.StructurePlacementData;
 import net.minecraft.structure.processor.StructureProcessor;
 import net.minecraft.structure.processor.StructureProcessorType;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
 
 public class LootChestProcessor extends StructureProcessor {
 	public static Codec<LootChestProcessor> CODEC = RecordCodecBuilder.create(instance -> instance.group(
 			BlockPos.CODEC.fieldOf("pos").forGetter(p -> p.chestPos),
+			Codec.BOOL.fieldOf("match_terrain").orElse(false).forGetter(p -> p.matchTerrain),
 			Codec.STRING.fieldOf("type").forGetter(p -> p.lootType)
 	).apply(instance, LootChestProcessor::new));
 
 	private final BlockPos chestPos;
+	private final boolean matchTerrain;
 	private final String lootType;
 
-	public LootChestProcessor(BlockPos chestPos, String lootType) {
+	public LootChestProcessor(BlockPos chestPos, boolean matchTerrain, String lootType) {
 		this.chestPos = chestPos;
+		this.matchTerrain = matchTerrain;
 		this.lootType = lootType;
 	}
 
@@ -34,9 +38,20 @@ public class LootChestProcessor extends StructureProcessor {
 		BlockPos pos = localInfo.pos;
 		if (pos.asLong() == this.chestPos.asLong()) {
 			// TODO: rotation
-			LootHelper.placeProviderChest((WorldAccess) world, worldInfo.pos, getLootProvider(this.lootType));
+
+			if (this.matchTerrain) {
+				int y = world.getTopY(Heightmap.Type.OCEAN_FLOOR_WG, worldInfo.pos.getX(), worldInfo.pos.getZ());
+
+				BlockPos topPos = new BlockPos(worldInfo.pos.getX(), y, worldInfo.pos.getZ());
+				LootHelper.placeProviderChest((WorldAccess) world, topPos, getLootProvider(this.lootType));
+			} else {
+				LootHelper.placeProviderChest((WorldAccess) world, worldInfo.pos, getLootProvider(this.lootType));
+			}
+
+			// Place air here
 			return null;
 		}
+
 
 		return worldInfo;
 	}
