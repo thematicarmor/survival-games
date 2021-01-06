20e383857dd21fbad85e7b3ed5fe1105affac555
SuperCoder7979 <k.pranav@gmail.com>
2021-01-06 11:57:02 -0500

    random crafting tables and furnaces in buildings

---
+++ src/main/java/supercoder79/survivalgames/game/map/gen/processor/ChanceAtProcessor.java
@@ -0,0 +1,69 @@
+package supercoder79.survivalgames.game.map.gen.processor;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import supercoder79.survivalgames.game.map.loot.LootHelper;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.state.property.Properties;
+import net.minecraft.structure.Structure;
+import net.minecraft.structure.StructurePlacementData;
+import net.minecraft.structure.processor.StructureProcessor;
+import net.minecraft.structure.processor.StructureProcessorType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+
+public class ChanceAtProcessor extends StructureProcessor {
+	public static Codec<ChanceAtProcessor> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+			BlockPos.CODEC.fieldOf("pos").forGetter(p -> p.pos),
+			BlockState.CODEC.fieldOf("state").forGetter(p -> p.state),
+			Codec.BOOL.fieldOf("match_terrain").orElse(false).forGetter(p -> p.matchTerrain),
+			Codec.DOUBLE.fieldOf("chance").forGetter(p -> p.chance)
+	).apply(instance, ChanceAtProcessor::new));
+
+	private final BlockPos pos;
+	private final boolean matchTerrain;
+	private final BlockState state;
+	private final double chance;
+
+	public ChanceAtProcessor(BlockPos pos, BlockState state, boolean matchTerrain, double chance) {
+		this.pos = pos;
+		this.matchTerrain = matchTerrain;
+		this.state = state;
+		this.chance = chance;
+	}
+
+	@Override
+	public Structure.StructureBlockInfo process(WorldView world, BlockPos worldPos, BlockPos localPos, Structure.StructureBlockInfo localInfo, Structure.StructureBlockInfo worldInfo, StructurePlacementData structurePlacementData) {
+		if (structurePlacementData.getRandom(worldInfo.pos).nextDouble() < this.chance) {
+			if (localInfo.pos.asLong() == this.pos.asLong()) {
+
+				BlockState state = this.state;
+				if (state.contains(Properties.FACING)) {
+					state = state.with(Properties.FACING, structurePlacementData.getRotation().rotate(state.get(Properties.FACING)));
+				} else if (state.contains(Properties.HORIZONTAL_FACING)) {
+					state = state.with(Properties.HORIZONTAL_FACING, structurePlacementData.getRotation().rotate(state.get(Properties.HORIZONTAL_FACING)));
+				}
+
+				if (this.matchTerrain) {
+					int y = world.getTopY(Heightmap.Type.OCEAN_FLOOR_WG, worldInfo.pos.getX(), worldInfo.pos.getZ());
+
+					BlockPos topPos = new BlockPos(worldInfo.pos.getX(), y, worldInfo.pos.getZ());
+					return new Structure.StructureBlockInfo(topPos, state, null);
+				}
+
+				return new Structure.StructureBlockInfo(worldInfo.pos, state, null);
+			}
+		}
+
+		return worldInfo;
+	}
+
+	@Override
+	protected StructureProcessorType<?> getType() {
+		return SurvivalGamesProcessorTypes.CHANCE_AT;
+	}
+}
--- src/main/java/supercoder79/survivalgames/game/map/gen/processor/SurvivalGamesProcessorTypes.java
+++ src/main/java/supercoder79/survivalgames/game/map/gen/processor/SurvivalGamesProcessorTypes.java
@@ -6,7 +6,9 @@ import net.minecraft.util.registry.Registry;
 
 public class SurvivalGamesProcessorTypes {
 	public static StructureProcessorType<LootChestProcessor> LOOT;
+	public static StructureProcessorType<ChanceAtProcessor> CHANCE_AT;
 	public static void init() {
 		LOOT = Registry.register(Registry.STRUCTURE_PROCESSOR, new Identifier("survivalgames", "loot"), () -> LootChestProcessor.CODEC);
+		CHANCE_AT = Registry.register(Registry.STRUCTURE_PROCESSOR, new Identifier("survivalgames", "chance_at"), () -> ChanceAtProcessor.CODEC);
 	}
 }
