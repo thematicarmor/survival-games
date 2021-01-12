5eaa2cbbcf388aa50b5953115398250b8bfe35db
SuperCoder7979 <k.pranav@gmail.com>
2021-01-11 21:35:14 -0500

    grand loot rebalance

--- src/main/java/supercoder79/survivalgames/game/map/loot/LootProviders.java
+++ src/main/java/supercoder79/survivalgames/game/map/loot/LootProviders.java
@@ -7,6 +7,7 @@ import net.minecraft.util.collection.WeightedList;
 public class LootProviders {
 
 	public static final LootProvider GENERIC = new LootProvider(new WeightedList<ItemStack>()
+			.add(new ItemStack(Items.WOODEN_SWORD, 1), 4)
 			.add(new ItemStack(Items.STONE_SWORD, 1), 16)
 			.add(new ItemStack(Items.STONE_AXE, 1), 4)
 			.add(new ItemStack(Items.SHIELD, 1), 3)
@@ -28,44 +29,59 @@ public class LootProviders {
 			4, 6);
 
 	public static final LootProvider HOUSE = new LootProvider(new WeightedList<ItemStack>()
+			.add(new ItemStack(Items.STONE_SWORD, 1), 6)
 			.add(new ItemStack(Items.IRON_SWORD, 1), 8)
 			.add(new ItemStack(Items.SHIELD, 1), 8)
 			.add(new ItemStack(Items.STONE_AXE, 1), 2)
 			.add(new ItemStack(Items.GOLDEN_APPLE, 1), 1)
+			.add(new ItemStack(Items.ENDER_PEARL, 1), 1)
 			.add(new ItemStack(Items.CHAINMAIL_HELMET, 1), 3)
 			.add(new ItemStack(Items.CHAINMAIL_CHESTPLATE, 1), 3)
 			.add(new ItemStack(Items.CHAINMAIL_LEGGINGS, 1), 3)
 			.add(new ItemStack(Items.CHAINMAIL_BOOTS, 1), 3)
+			.add(new ItemStack(Items.LEATHER_HELMET, 1), 2)
+			.add(new ItemStack(Items.LEATHER_CHESTPLATE, 1), 2)
+			.add(new ItemStack(Items.LEATHER_LEGGINGS, 1), 2)
+			.add(new ItemStack(Items.LEATHER_BOOTS, 1), 2)
 			.add(new ItemStack(Items.IRON_HELMET, 1), 1)
 			.add(new ItemStack(Items.IRON_CHESTPLATE, 1), 1)
 			.add(new ItemStack(Items.IRON_LEGGINGS, 1), 1)
 			.add(new ItemStack(Items.IRON_BOOTS, 1), 1),
-			4, 8);
+			4, 6);
 
 	public static final LootProvider ENCHANTING_TABLE = new LootProvider(new WeightedList<ItemStack>()
+			.add(new ItemStack(Items.STONE_SWORD, 1), 1)
 			.add(new ItemStack(Items.IRON_SWORD, 1), 1)
 			.add(new ItemStack(Items.SHIELD, 1), 1)
+			.add(new ItemStack(Items.ENDER_PEARL, 1), 2)
 			.add(new ItemStack(Items.EXPERIENCE_BOTTLE, 4), 4)
 			.add(new ItemStack(Items.LAPIS_LAZULI, 2), 4)
 			.add(new ItemStack(Items.CHAINMAIL_HELMET, 1), 1)
 			.add(new ItemStack(Items.CHAINMAIL_CHESTPLATE, 1), 1)
 			.add(new ItemStack(Items.CHAINMAIL_LEGGINGS, 1), 1)
-			.add(new ItemStack(Items.CHAINMAIL_BOOTS, 1), 1),
-			4, 8);
+			.add(new ItemStack(Items.CHAINMAIL_BOOTS, 1), 1)
+			.add(new ItemStack(Items.LEATHER_HELMET, 1), 1)
+			.add(new ItemStack(Items.LEATHER_CHESTPLATE, 1), 1)
+			.add(new ItemStack(Items.LEATHER_LEGGINGS, 1), 1)
+			.add(new ItemStack(Items.LEATHER_BOOTS, 1), 1),
+			4, 6);
 
 	public static final LootProvider TOWER = new LootProvider(new WeightedList<ItemStack>()
+			.add(new ItemStack(Items.STONE_SWORD, 1), 1)
 			.add(new ItemStack(Items.IRON_SWORD, 1), 2)
 			.add(new ItemStack(Items.STONE_AXE, 1), 2)
 			.add(new ItemStack(Items.SHIELD, 1), 1)
 			.add(new ItemStack(Items.BOW, 1), 3)
 			.add(new ItemStack(Items.ARROW, 16), 4)
+			.add(new ItemStack(Items.ENDER_PEARL, 1), 1)
 			.add(new ItemStack(Items.CHAINMAIL_HELMET, 1), 1)
 			.add(new ItemStack(Items.CHAINMAIL_CHESTPLATE, 1), 1)
 			.add(new ItemStack(Items.CHAINMAIL_LEGGINGS, 1), 1)
 			.add(new ItemStack(Items.CHAINMAIL_BOOTS, 1), 1),
-			4, 8);
+			4, 6);
 
 	public static final LootProvider ORE_PILE = new LootProvider(new WeightedList<ItemStack>()
+			.add(new ItemStack(Items.STONE_SWORD, 1), 6)
 			.add(new ItemStack(Items.IRON_SWORD, 1), 12)
 			.add(new ItemStack(Items.SHIELD, 1), 8)
 			.add(new ItemStack(Items.STONE_AXE, 1), 4)
@@ -83,9 +99,10 @@ public class LootProviders {
 			.add(new ItemStack(Items.IRON_CHESTPLATE, 1), 1)
 			.add(new ItemStack(Items.IRON_LEGGINGS, 1), 1)
 			.add(new ItemStack(Items.IRON_BOOTS, 1), 1),
-			4, 7);
+			4, 6);
 
 	public static final LootProvider FARMLAND = new LootProvider(new WeightedList<ItemStack>()
+			.add(new ItemStack(Items.STONE_SWORD, 1), 8)
 			.add(new ItemStack(Items.IRON_SWORD, 1), 6)
 			.add(new ItemStack(Items.SHIELD, 1), 6)
 			.add(new ItemStack(Items.GOLDEN_APPLE, 1), 1)
@@ -96,5 +113,5 @@ public class LootProviders {
 			.add(new ItemStack(Items.CHAINMAIL_CHESTPLATE, 1), 1)
 			.add(new ItemStack(Items.CHAINMAIL_LEGGINGS, 1), 1)
 			.add(new ItemStack(Items.CHAINMAIL_BOOTS, 1), 1),
-			4, 7);
+			4, 6);
 }
