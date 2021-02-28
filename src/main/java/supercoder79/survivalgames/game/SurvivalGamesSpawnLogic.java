e7a07d8412905174efe27a489fcfaa58da277482
SuperCoder7979 <25208576+SuperCoder7979@users.noreply.github.com>
2021-02-28 11:33:58 -0500

    Spiral outwards to avoid spawning players in liquid

--- src/main/java/supercoder79/survivalgames/game/SurvivalGamesSpawnLogic.java
+++ src/main/java/supercoder79/survivalgames/game/SurvivalGamesSpawnLogic.java
@@ -45,6 +45,37 @@ public final class SurvivalGamesSpawnLogic {
         WorldChunk chunk = world.getChunk(chunkPos.x, chunkPos.z);
         BlockPos pos = new BlockPos(x, chunk.sampleHeightmap(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, x, z) + 1, z);
 
+        if (!chunk.getBlockState(pos.down()).getFluidState().isEmpty()) {
+            System.out.println("Avoiding fluid");
+            boolean found = false;
+
+            // Try 20 times to spiral outwards, hopefully not hitting fluid
+            for (int i = 0; i < 20; i++) {
+                if (found) {
+                    break;
+                }
+
+                int dist = i * 8 + 16;
+
+                int count = 4 + i;
+                for (int j = 0; j < count; j++) {
+                    double theta = ((double)j / count) * Math.PI * 2;
+                    int ax = (int) (Math.cos(theta) * dist) + x;
+                    int az = (int) (Math.sin(theta) * dist) + z;
+
+                    ChunkPos circlePos = new ChunkPos(ax >> 4, az >> 4);
+                    WorldChunk circleChunk = world.getChunk(circlePos.x, circlePos.z);
+                    pos = new BlockPos(ax, circleChunk.sampleHeightmap(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, ax, az) + 1, az);
+
+                    // Check the position at the circle
+                    if (chunk.getBlockState(pos.down()).getFluidState().isEmpty()) {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+        }
+
         player.teleport(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 0.0F, 0.0F);
     }
 }
